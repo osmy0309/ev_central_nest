b9a1d8d39877e7df54fbee98e02a480890a68efa
osmy0309 <osmy0309@gmail.com>
2023-07-17 11:56:55 -0400

    new swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API EV CENTRAL NEST')
+    .setDescription('')
+    .setVersion('1.0')
+    .addTag('connect OCPP')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
   await app.listen(3800);
 }
 bootstrap();
