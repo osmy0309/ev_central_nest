de5aae24858f2e22ace6102ed3ae722a43dc35c8
osmy0309 <osmy0309@gmail.com>
2023-09-17 19:51:25 +0200

    new change in guards

--- src/guards/token.guard.ts
+++ src/guards/token.guard.ts
@@ -1,16 +1,19 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { Reflector } from '@nestjs/core';
-import { AuthService } from './auth.service';
+import { AuthOauthService } from '../auth-oauth/auth-oauth.service';
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private reflector: Reflector) {}
+  constructor(
+    private authService: AuthOauthService,
+    private reflector: Reflector,
+  ) {}
 
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
     // Aquí puedes realizar la lógica de autenticación utilizando el AuthService
-    return this.authService.isAuthenticated(request);
+    return this.authService.validateToken(request);
   }
 }
