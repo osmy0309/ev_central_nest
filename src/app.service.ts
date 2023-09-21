e7e7150121be4afb3d79685e526ac1deb0879912
Osmin Garcia Prieto <109307243+osmy0309@users.noreply.github.com>
2023-09-21 21:58:01 +0200

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,9 +4,9 @@ import { OcppService } from './ocpp/ocpp.service';
 @Injectable()
 export class AppService {
   constructor(private ocppService: OcppService) {}
-  async onApplicationBootstrap() {
+  /* async onApplicationBootstrap() {
     await this.ocppService.startServer();
-  }
+  }*/
   async getHello(): Promise<string> {
     // await this.ocppService.startClient();
     return 'Hello World2!';
